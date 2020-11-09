9fbfab5de3dccad34e92d4f15f600a28c80b0a8c
Heghine Aghajanyan <73738790+heghineh@users.noreply.github.com>
2020-11-09 11:03:12 +0400

    Update Problem2_2.java

--- src/com/company/Problem2_2.java
+++ src/com/company/Problem2_2.java
@@ -14,7 +14,6 @@ public class Problem2_2 {
         if (sum % 2 == 0) {
             Long sum1 = (long) sum;
             System.out.println(number1 + " + " + number2 + " = " + sum1 + "\nCasted to long!");
-
         } else {
             Double sum2 = (double) sum;
             System.out.println(number1 + " + " + number2 + " = " + sum2 + "\nCasted to double!");
