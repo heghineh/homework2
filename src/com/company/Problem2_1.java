38f6ebe1a9a7033685b0624a4b91ea34c03864ce
Heghine Aghajanyan <73738790+heghineh@users.noreply.github.com>
2020-11-09 10:35:44 +0400

    Update Problem2_1.java

--- src/com/company/Problem2_1.java
+++ src/com/company/Problem2_1.java
@@ -1,7 +1,19 @@
 package com.company;
 
+import java.util.Random;
+
 public class Problem2_1 {
     public static void main(String[] args) {
-        System.out.println("myau");
+        Random random = new Random();
+
+        int number1 = random.nextInt(100) + 10;
+        System.out.println("Number 1 is " + number1);
+        int number2 = random.nextInt(100) + 10;
+        System.out.println("Number 2 is " + number2);
+
+        int sum = number1 + number2;
+        Long Sum = (long) sum;
+
+        System.out.println(number1 + " + " + number2 + " = " + Sum);
     }
 }
